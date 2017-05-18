9297375d425382aca9bb1a13fb811ce6ef0c6fbf
l30n13 <mrtonmoy12@gmail.com>
2017-05-18 16:30:23 +0600

    Modified

--- app/src/main/java/com/humaclab/selliscope/model/OrderResponse.java
+++ app/src/main/java/com/humaclab/selliscope/model/OrderResponse.java
@@ -15,25 +15,29 @@ public class OrderResponse implements Serializable {
     @SerializedName("result")
     public OrderResult result;
 
-    public static class OrderResult {
+    public static class OrderResult implements Serializable {
         @SerializedName("orders")
         public List<OrderList> orderList;
     }
 
-    public static class OrderList {
+    public static class OrderList implements Serializable {
         @SerializedName("id")
         public int orderId;
-        @SerializedName("date")
-        public String orderDate;
         @SerializedName("outlet_id")
         public int outletId;
         @SerializedName("outlet_name")
         public String outletName;
+        @SerializedName("discount")
+        public String discount;
+        @SerializedName("amount")
+        public String amount;
+        @SerializedName("order_date")
+        public String orderDate;
         @SerializedName("products")
         public List<Product> productList;
     }
 
-    public static class Product {
+    public static class Product implements Serializable {
         @SerializedName("id")
         public int productId;
         @SerializedName("name")

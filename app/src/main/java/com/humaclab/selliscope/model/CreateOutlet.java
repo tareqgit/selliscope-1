906e8f2ae1cca08083c14209b8c93963eb7e4e43
l30n13 <mrtonmoy12@gmail.com>
2017-05-30 13:10:20 +0600

    Updated

--- app/src/main/java/com/humaclab/selliscope/model/CreateOutlet.java
+++ app/src/main/java/com/humaclab/selliscope/model/CreateOutlet.java
@@ -11,60 +11,41 @@ import java.util.List;
  */
 
 public class CreateOutlet {
-    @SerializedName("outlet")
-    List<Outlet> outlets;
+    @SerializedName("type_id")
+    public double outletTypeId;
+    @SerializedName("name")
+    public String outletName;
+    @SerializedName("owner")
+    public String ownerName;
+    @SerializedName("address")
+    public String address;
+    @SerializedName("thana_id")
+    public int outletThanaId;
+    @SerializedName("phone")
+    public String outletPhoneNumber;
+    @SerializedName("latitude")
+    public double outletLatitude;
+    @SerializedName("longitude")
+    public double outletLongitude;
+    @SerializedName("img")
+    public String outletImage;
 
-    public CreateOutlet(List<Outlet> outlets) {
-        this.outlets = outlets;
-
+    public CreateOutlet(int outletTypeId, String outletName, String ownerName, String address,
+                        int outletThanaId, String outletPhoneNumber, double outletLatitude,
+                        double outletLongitude, String outletImage) {
+        this.outletTypeId = outletTypeId;
+        this.outletName = outletName;
+        this.ownerName = ownerName;
+        this.address = address;
+        this.outletThanaId = outletThanaId;
+        this.outletPhoneNumber = outletPhoneNumber;
+        this.outletLatitude = outletLatitude;
+        this.outletLongitude = outletLongitude;
+        this.outletImage = outletImage;
     }
 
-    public static class Outlet {
-        public Outlet(int outletTypeId, String outletName, String ownerName, String address,
-                      int outletThanaId, String outletPhoneNumber, double outletLatitude,
-                      double outletLongitude, String outletImage) {
-            this.outletTypeId = outletTypeId;
-            this.outletName = outletName;
-            this.ownerName = ownerName;
-            this.address = address;
-            this.outletThanaId = outletThanaId;
-            this.outletPhoneNumber = outletPhoneNumber;
-            this.outletLatitude = outletLatitude;
-            this.outletLongitude = outletLongitude;
-            this.outletImage = outletImage;
-        }
-
-        @SerializedName("type_id")
-        public double outletTypeId;
-        @SerializedName("name")
-        public String outletName;
-        @SerializedName("owner")
-        String ownerName;
-        @SerializedName("address")
-        String address;
-        @SerializedName("thana_id")
-        int outletThanaId;
-        @SerializedName("phone")
-        String outletPhoneNumber;
-        @SerializedName("latitude")
-        double outletLatitude;
-        @SerializedName("longitude")
-        double outletLongitude;
-        @SerializedName("img")
-        String outletImage;
-    }
-
-    public static class Successful {
-        @SerializedName("error")
-        public boolean error;
-        @SerializedName("result")
-        public String result;
-    }
-
-    public static class Unsuccessful {
-        @SerializedName("error")
-        public boolean error;
-        @SerializedName("result")
-        public String result;
-    }
+    @SerializedName("error")
+    public boolean error;
+    @SerializedName("result")
+    public String result;
 }

f714673e237e086bcdd3efdd4131b63b2afd4f1e
l30n13 <mrtonmoy12@gmail.com>
2017-08-30 12:55:57 +0600

    Timber log added

--- app/src/main/java/com/humaclab/selliscope/Receiver/StartBootReceiver.java
+++ app/src/main/java/com/humaclab/selliscope/Receiver/StartBootReceiver.java
@@ -3,9 +3,10 @@ package com.humaclab.selliscope.Receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import com.humaclab.selliscope.Service.SendLocationDataService;
+
+import timber.log.Timber;
 
 /**
  * Created by leon on 8/23/17.
@@ -17,7 +18,7 @@ public class StartBootReceiver extends BroadcastReceiver {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
             Intent activityIntent = new Intent(context, SendLocationDataService.class);
             context.startService(activityIntent);
-            Log.d("BroadCast Receiver", "Service start command sent.");
+            Timber.d("BroadCast Receiver: Service start command sent.");
         }
     }
 }

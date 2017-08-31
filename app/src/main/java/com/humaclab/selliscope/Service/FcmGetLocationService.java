ad601138c2268ba5178e35c469800137a3a61e57
l30n13 <mrtonmoy12@gmail.com>
2017-08-31 17:03:57 +0600

    Location code took into one place

--- app/src/main/java/com/humaclab/selliscope/Service/FcmGetLocationService.java
+++ app/src/main/java/com/humaclab/selliscope/Service/FcmGetLocationService.java
@@ -3,6 +3,7 @@ package com.humaclab.selliscope.Service;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.google.gson.Gson;
+import com.humaclab.selliscope.Utils.SendUserLocationData;
 
 /**
  * Created by leon on 8/28/17.
@@ -12,6 +13,11 @@ public class FcmGetLocationService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+
+        SendUserLocationData sendUserLocationData = new SendUserLocationData(getApplicationContext());
+        if (sendUserLocationData.getLocation()) {
+            sendUserLocationData.getInstantLocation();
+        }
 
         System.out.println("Firebase return: " + new Gson().toJson(remoteMessage));
     }

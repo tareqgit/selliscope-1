c706ece487ec0236aafb8666e1f8e9aa843f4a3c
l30n13 <mrtonmoy12@gmail.com>
2017-10-10 13:34:36 +0600

    Commented some code for temporarily

--- app/src/main/java/com/humaclab/selliscope/Service/FcmGetLocationService.java
+++ app/src/main/java/com/humaclab/selliscope/Service/FcmGetLocationService.java
@@ -3,7 +3,6 @@ package com.humaclab.selliscope.Service;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.google.gson.Gson;
-import com.humaclab.selliscope.Utils.SendUserLocationData;
 
 /**
  * Created by leon on 8/28/17.
@@ -14,10 +13,10 @@ public class FcmGetLocationService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
-        SendUserLocationData sendUserLocationData = new SendUserLocationData(getApplicationContext());
+        /*SendUserLocationData sendUserLocationData = new SendUserLocationData(getApplicationContext());
         if (sendUserLocationData.getLocation()) {
             sendUserLocationData.getInstantLocation();
-        }
+        }*/
 
         System.out.println("Firebase return: " + new Gson().toJson(remoteMessage));
     }

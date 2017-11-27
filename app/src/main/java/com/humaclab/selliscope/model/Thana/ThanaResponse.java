f0760ddf584890e9bd8328034ec87adf6288883e
l30n13 <mrtonmoy12@gmail.com>
2017-11-27 19:47:13 +0600

    Previous model removed and new model added

---
+++ app/src/main/java/com/humaclab/selliscope/model/Thana/ThanaResponse.java
@@ -0,0 +1,28 @@
+package com.humaclab.selliscope.model.Thana;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ThanaResponse {
+
+    @SerializedName("result")
+    private Result result;
+
+    @SerializedName("error")
+    private boolean error;
+
+    public Result getResult() {
+        return result;
+    }
+
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+    public boolean isError() {
+        return error;
+    }
+
+    public void setError(boolean error) {
+        this.error = error;
+    }
+}

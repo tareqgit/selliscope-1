828314c757b310f83add740799f22fd59382a414
l30n13 <mrtonmoy12@gmail.com>
2017-12-31 12:19:15 +0600

    API link changed

--- app/src/main/java/com/humaclab/selliscope_2fa/utils/Constants.java
+++ app/src/main/java/com/humaclab/selliscope_2fa/utils/Constants.java
@@ -5,9 +5,7 @@ package com.humaclab.selliscope_2fa.utils;
  */
 
 public class Constants {
-    //    public static final String BASE_URL = "http://139.59.107.229/api/v1/";
-//    public static final String BASE_URL = "http://devsnet.com/api/v1/";
-    public static final String BASE_URL = "http://app.selliscope.com/api/v1/";
+    public static final String BASE_URL = "http://2fa.selliscope.com/api/v1/";
     public static final String userAgent = System.getProperty("http.agent");
     public static final String databaseName = "selliscopedb";
 }

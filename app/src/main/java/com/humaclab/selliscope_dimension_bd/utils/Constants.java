e2de4935159e319382c36d38de1126ade8fce322
l30n13 <mrtonmoy12@gmail.com>
2018-01-21 16:48:22 +0600

    API link changed for Dimension BD

--- app/src/main/java/com/humaclab/selliscope_dimension_bd/utils/Constants.java
+++ app/src/main/java/com/humaclab/selliscope_dimension_bd/utils/Constants.java
@@ -5,9 +5,7 @@ package com.humaclab.selliscope_dimension_bd.utils;
  */
 
 public class Constants {
-    //    public static final String BASE_URL = "http://139.59.107.229/api/v1/";
-//    public static final String BASE_URL = "http://devsnet.com/api/v1/";
-    public static final String BASE_URL = "http://app.selliscope.com/api/v1/";
+    public static final String BASE_URL = "http://dbd.selliscope.com/api/v1/";
     public static final String userAgent = System.getProperty("http.agent");
     public static final String databaseName = "selliscopedb";
 }

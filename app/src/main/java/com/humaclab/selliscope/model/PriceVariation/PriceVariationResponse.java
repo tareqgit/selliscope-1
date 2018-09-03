1ae0ed1c81be82fb0e6380dfa52ff34721782615
sobin <anam.uits@gmail.com>
2018-09-03 13:26:02 +0600

    Price Variation Model for API

---
+++ app/src/main/java/com/humaclab/selliscope/model/PriceVariation/PriceVariationResponse.java
@@ -0,0 +1,33 @@
+package com.humaclab.selliscope.model.PriceVariation;
+
+import com.google.gson.annotations.SerializedName;
+
+
+/**
+ * Created by anam on 29/7/2018.
+ */
+
+public class PriceVariationResponse {
+
+    @SerializedName("result")
+    private Result result;
+
+    @SerializedName("error")
+    private boolean error;
+
+    public Result getResult() {
+        return result;
+    }
+
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+    public boolean isError() {
+        return error;
+    }
+
+    public void setError(boolean error) {
+        this.error = error;
+    }
+}

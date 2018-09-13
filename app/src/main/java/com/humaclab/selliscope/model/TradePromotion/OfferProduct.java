08284d949df668abcfa723fa327ba78a23a66711
sobin <anam.uits@gmail.com>
2018-09-13 15:10:13 +0600

    trade promotion Model

---
+++ app/src/main/java/com/humaclab/selliscope/model/TradePromotion/OfferProduct.java
@@ -0,0 +1,39 @@
+package com.humaclab.selliscope.model.TradePromotion;
+
+import com.google.gson.annotations.SerializedName;
+
+public class OfferProduct {
+
+
+    private Integer productId;
+    @SerializedName("product_name")
+
+    private String productName;
+    @SerializedName("product_qty")
+
+    private Integer productQty;
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getProductQty() {
+        return productQty;
+    }
+
+    public void setProductQty(Integer productQty) {
+        this.productQty = productQty;
+    }
+}
---
+++ app/src/main/java/com/humaclab/selliscope/model/TradePromotion/Promotion.java
@@ -0,0 +1,76 @@
+package com.humaclab.selliscope.model.TradePromotion;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Promotion {
+
+    @SerializedName("promotion_title")
+    private String promotionTitle;
+
+    @SerializedName("promotion_type")
+    private String promotionType;
+
+    @SerializedName("promotion_value")
+
+    private Integer promotionValue;
+    @SerializedName("offer_type")
+
+    private String offerType;
+    @SerializedName("offer_value")
+
+    private Integer offerValue;
+    @SerializedName("offer_product")
+
+    private List<OfferProduct> offerProduct = null;
+
+    public String getPromotionTitle() {
+        return promotionTitle;
+    }
+
+    public void setPromotionTitle(String promotionTitle) {
+        this.promotionTitle = promotionTitle;
+    }
+
+    public String getPromotionType() {
+        return promotionType;
+    }
+
+    public void setPromotionType(String promotionType) {
+        this.promotionType = promotionType;
+    }
+
+    public Integer getPromotionValue() {
+        return promotionValue;
+    }
+
+    public void setPromotionValue(Integer promotionValue) {
+        this.promotionValue = promotionValue;
+    }
+
+    public String getOfferType() {
+        return offerType;
+    }
+
+    public void setOfferType(String offerType) {
+        this.offerType = offerType;
+    }
+
+    public Integer getOfferValue() {
+        return offerValue;
+    }
+
+    public void setOfferValue(Integer offerValue) {
+        this.offerValue = offerValue;
+    }
+
+    public List<OfferProduct> getOfferProduct() {
+        return offerProduct;
+    }
+
+    public void setOfferProduct(List<OfferProduct> offerProduct) {
+        this.offerProduct = offerProduct;
+    }
+
+}

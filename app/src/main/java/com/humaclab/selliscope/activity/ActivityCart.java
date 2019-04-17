146fce5f8b6a1c281e7bb270a2ae2269fa4495ce
tareq3 <mti.tareq3@gmai.com>
2019-04-17 16:22:06 +0600

    if net not available que the task and get back to outlet activity

--- app/src/main/java/com/humaclab/selliscope/activity/ActivityCart.java
+++ app/src/main/java/com/humaclab/selliscope/activity/ActivityCart.java
@@ -199,7 +199,7 @@ public class ActivityCart extends AppCompatActivity implements OnSelectProduct {
                             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                             startActivity(intent);
 
-                            //finish();
+                            finish();
                             //startActivity(new Intent(ActivityCart.this, OutletActivity.class));
                         } else if (response.code() == 401) {
                             System.out.println(new Gson().toJson(response.body()));
@@ -220,6 +220,12 @@ public class ActivityCart extends AppCompatActivity implements OnSelectProduct {
                 databaseHandler.setOrder(addNewOrder);
                 Toast.makeText(ActivityCart.this, "Order created successfully", Toast.LENGTH_LONG).show();
                 pd.dismiss();
+                //clear selected Item list
+                selectedProductList.clear();
+                //if net is not available que the task and get back to Outlet
+                Intent intent = new Intent(getApplicationContext(), OutletActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
                 finish();
             }
         } catch (Exception e) {

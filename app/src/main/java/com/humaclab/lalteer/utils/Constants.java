de12d33b60b718235d2d361c319e59b2e2907695
tareq3 <mti.tareq3@gmail.com>
2019-09-05 10:42:39 +0600

    Url updated

--- app/src/main/java/com/humaclab/lalteer/utils/Constants.java
+++ app/src/main/java/com/humaclab/lalteer/utils/Constants.java
@@ -7,9 +7,9 @@ package com.humaclab.lalteer.utils;
 public class Constants {
     //    public static final String BASE_URL = "http://139.59.107.229/api/v1/";
 //    public static final String BASE_URL = "http://devsnet.com/api/v1/";
-    public static final String BASE_URL_HTTP = "http://139.59.107.229/api/";
-    public static final String BASE_URL_HTTPS = "https://139.59.107.229/api/";
-    public static  String BASE_URL = "http://139.59.107.229/api/";
+    public static final String BASE_URL_HTTP = "http://157.245.110.92/api/";
+    public static final String BASE_URL_HTTPS = "https://157.245.110.92/api/";
+    public static  String BASE_URL = "http://157.245.110.92/api/";
     public static final String baseUrl = "http://139.59.107.229/";
     public static final String userAgent = System.getProperty("http.agent");
     public static final String databaseName = "lalteer";

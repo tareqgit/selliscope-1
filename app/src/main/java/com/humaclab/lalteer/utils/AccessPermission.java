7f0e493d29881478e4f5fcbe0f0de603f85df6ab
Tareq Islam <mti.tareq3@gmail.com>
2019-10-14 17:22:57 +0600

    some changes for android !

--- app/src/main/java/com/humaclab/lalteer/utils/AccessPermission.java
+++ app/src/main/java/com/humaclab/lalteer/utils/AccessPermission.java
@@ -3,6 +3,9 @@ package com.humaclab.lalteer.utils;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 
 /**
@@ -10,19 +13,24 @@ import androidx.core.app.ActivityCompat;
  */
 
 public class AccessPermission {
+
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     public static void accessPermission(Activity activity) {
         String[] permissions = {
-                Manifest.permission.CAMERA,
                 Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.CAMERA,
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.CALL_PHONE
+                Manifest.permission.CALL_PHONE,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
         };
-        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED
+        if (       ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED
                 || ActivityCompat.checkSelfPermission(activity, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
                 || ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 || ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                || ActivityCompat.checkSelfPermission(activity, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                || ActivityCompat.checkSelfPermission(activity, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(activity, permissions, 404);
         }
     }
